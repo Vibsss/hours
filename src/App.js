01461b386c25cf91f908186dd0a5287a7fd20c8b
Hardik Silawat <hardiksilawat0902@.gmail.com>
2024-04-27 23:26:15 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
           <Routes>
             <Route
               exact
-              path="/"
+              path="/hour"
               element={
                 <TestForm
                   paddingVal="my-3"
